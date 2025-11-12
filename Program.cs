0f52f65928c6d46d9f7f7ad1d4b5c3e99177d9e9
mateushrc <mat3us.hrc@gmail.com>
2025-11-12 19:54:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidateEmail
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an email address to validate:");
+            string? email = Console.ReadLine();
+
+            if (IsValidEmail(email!))
+            {
+                Console.WriteLine("The email address is valid.");
+            }
+            else
+            {
+                Console.WriteLine("The email address is invalid.");
+            }
+        }
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                Regex emailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);
+                return emailRegex.IsMatch(email);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
